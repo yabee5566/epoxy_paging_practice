1f9a626bd86533f3f17ea929f01a3a9cb17c0f11
Ian Pan <g-hamu@hotmail.com>
2021-04-27 10:42:49 +0800

    make things look better

--- app/src/main/java/com/example/epoxypagingpractice/epoxy/EpoxyPagingController.kt
+++ app/src/main/java/com/example/epoxypagingpractice/epoxy/EpoxyPagingController.kt
@@ -24,17 +24,17 @@ class EpoxyPagingController : PagingDataEpoxyController<TicketData>() {
         super.addModels(models)
         if (state?.append is LoadState.Loading){
             loadStateModelView {
-                id(13123)
                 loadState(LoadState.Loading)
-                retryListener { retry() }
+                id("loading")
             }
+
         }
-        else if (state?.prepend is LoadState.Error || state?.append is LoadState.Error || state?.refresh is LoadState.Error ){
+        else if (state?.prepend is LoadState.Error || state?.append is LoadState.Error || state?.refresh is LoadState.Error ) {
             Log.d("EpoxyPagingController", "error clause")
             loadStateModelView {
-                id(13123)
-                loadState(LoadState.Error(Throwable("eeeeeeeeeeeeeee")))
-                retryListener { retry() }
+                id("error")
+                loadState(LoadState.Error(Throwable("something went wrong")))
+                .retryListener { retry() }
             }
         }
     }
